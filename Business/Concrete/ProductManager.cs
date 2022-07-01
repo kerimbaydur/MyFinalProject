4bd34e4d069913c043cb53d594c2c51d74b546de
busranurbaydur <busranurbaydur@outlook.com>
2022-07-02 01:13:15 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _inProductDal;
+
+        public ProductManager(IProductDal inProductDal)
+        {
+            _inProductDal = inProductDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları;
+            return _inProductDal.GetAll();
+
+
+
+
+        }
+    }
+}
