a242369467c7dea4be964cf99086b86070b5552e
busranurbaydur <busranurbaydur@outlook.com>
2022-07-02 14:29:20 +0300

    Commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -12,22 +12,31 @@ namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-        IProductDal _inProductDal;
+        IProductDal _productDal;
 
-        public ProductManager(IProductDal inProductDal)
+        public ProductManager(IProductDal productDal)
         {
-            _inProductDal = inProductDal;
+            _productDal = productDal;
         }
 
         public List<Product> GetAll()
         {
             //İş kodları;
-            return _inProductDal.GetAll();
-            //kerims
+            return _productDal.GetAll();
+            
 
 
 
+        }
 
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
